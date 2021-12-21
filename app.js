11ba19c4ad055c776d5de217c853624fad53ed2c
Austin Harkey <austindharkey@gmail.com>
2021-12-21 11:26:16 -0800

    basic mechs working

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-let gameBoard = (() => {
-    let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    
+let game = (() => {
+    let board = [, , , , , , , , ];
+    let player = 'x';
+    let boardTiles = document.querySelectorAll('.game__tile');
+    let getTileIndex = (e) => {
+        let i =  e.target.dataset.index;
+        if (typeof board[i] == 'undefined') {
+            if(player == 'x') {
+                board[i] = 'x';
+                e.target.textContent = board[i];
+                player = 'o';
+            } else {
+                board[i] = 'o';
+                e.target.textContent = board[i];
+                player = 'x';
+            }
+            console.log(board);
+        } else {
+            return;
+        }
+    }
+    let tiles = boardTiles.forEach((tile, idx) => {
+        tile.dataset.index = idx;
+        tile.addEventListener('click', getTileIndex);
+    });
 })();
+
